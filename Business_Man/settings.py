ee3f6ede7319043076096923bd0e749317b25175
Euan McKerrow <emckerrow1@gmail.com>
2017-08-09 23:00:12 +0100

    Added SVG and more HTML formatting

--- Business_Man/settings.py
+++ Business_Man/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import mimetypes
 
+mimetypes.add_type("image/svg+xml", ".svg", True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
